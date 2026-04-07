1d0325b5a7ec7d189349c607dae7f1cece536edd
LDKOfficial <Lasse@kjeld-gaard.dk>
2026-04-07 14:42:39 +0200

    enemies walk animation
    Left work right not work

--- DDU Eksamen/Assets/Scripts/CameraMovement.cs
+++ DDU Eksamen/Assets/Scripts/CameraMovement.cs
@@ -7,19 +7,47 @@ public class CameraMovement : MonoBehaviour
 
     new private Rigidbody2D rigidbody;
 
-    public int cameraSpeed = 1;
+    public int cameraSpeed = 6;
+
+    public int cameraSpeedFast = 12;
+
+    private int cameraSpeedMultiplier;
+
+    private bool isFast = false;
+
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        cameraSpeedMultiplier = cameraSpeed;
     }
 
     private void Update()
     {
-        rigidbody.linearVelocity = movementInput * cameraSpeed;
+        rigidbody.linearVelocity = movementInput * cameraSpeedMultiplier;
     }
 
     public void Move(InputAction.CallbackContext context)
     {
         movementInput = context.ReadValue<Vector2>();
     }
+
+    public void SpeedUp(InputAction.CallbackContext context)
+    {
+
+        if (context.started)
+            return;
+        else if (!isFast)
+        {
+            cameraSpeedMultiplier = cameraSpeedFast;
+
+            isFast = true;
+        }
+        else
+        {
+            cameraSpeedMultiplier = cameraSpeed;
+
+            isFast = false;
+        }
+    }
+
 }
